2c72e5a193c3da143467526bac1f40c56082565d
Kanisorn Pitakwong <kanisornpitakwong@gmail.com>
2020-08-06 07:14:47 +0700

    form testing

--- pages/student/testing/index.vue
+++ pages/student/testing/index.vue
@@ -1,8 +1,22 @@
 <template>
-  <div>
+  <v-app>
       <dashheader :firstname="name" status="Student"/>
-      
-  </div>
+        <v-card v-for="data in datas" :key="data.id" class="d-flex pl-5 mb-2">
+            <v-row justify="center"> 
+                <v-col sm="8" class="pb-0">
+                    <v-text-field label="Question" v-model="data.text" outlined required></v-text-field>
+                    <v-text-field label="Level" v-model="data.level" outlined required></v-text-field>
+                    <v-row v-for="choice in data.arrChoice" :key="choice.id" justify="center"> 
+                        <v-checkbox></v-checkbox>
+                        <v-text-field v-model="choice.text"></v-text-field>
+                    </v-row>
+                </v-col>
+            </v-row>
+        </v-card>
+        <v-row justify="center" class="mt-5">
+            <v-btn color="success" class="mx-3" width="200px" height="50px">Submit!</v-btn>
+        </v-row>
+  </v-app>
 </template>
 
 <script>
